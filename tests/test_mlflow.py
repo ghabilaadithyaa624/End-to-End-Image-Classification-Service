e3438a00ce52eb989bf52eeee61ed05cb881fcff
ghabilaadithyaa624 <133510254+ghabilaadithyaa624@users.noreply.github.com>
2026-09-19 10:54:24 +0530

    feat(tests): add test_mlflow.py to verify MLflow server connection

---
+++ tests/test_mlflow.py
@@ -0,0 +1,23 @@
+import mlflow
+
+
+def test_mlflow_connection():
+
+    mlflow.set_tracking_uri(
+        "http://127.0.0.1:5000"
+    )
+
+    experiment_name = "Image Classification"
+
+    experiment = mlflow.get_experiment_by_name(
+        experiment_name
+    )
+
+    if experiment is None:
+        experiment_id = mlflow.create_experiment(
+            experiment_name
+        )
+    else:
+        experiment_id = experiment.experiment_id
+
+    assert experiment_id is not None
